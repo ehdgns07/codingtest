c83624f3933bef9420754d6716dc0e91b795b6b7
liverbird <ehdgns07@gmail.com>
2022-06-24 23:36:45 +0900

    array complete

--- src/array/array.java
+++ src/array/array.java
@@ -1,33 +1,39 @@
 package array;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
 
 public class array {
     public static void main(String[] args) {
-        int[][] arr1 = { { 1 }, { 2 } };
-        int[][] arr2 = { { 3 }, { 4 } };
+        int[][] arr1 = { { 1 },
+                         { 2 } };
+        int[][] arr2 = { { 1 },
+                         { 2 } };
         solution(arr1, arr2);
     }
 
     public static int[][] solution(int[][] arr1, int[][] arr2) {
 
-        int[][] answer = new int[arr1.length][arr1.length];
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        int count = arr1.length;
+        int count2 = arr1[0].length;
 
-        long count1 = (int) Arrays.stream(arr1).count();
-        long count2 = (int) Arrays.stream(arr1).count();
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count2; j++) {
+
+                    answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
 
         for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                answer[i][j] = arr1[i][j] + arr2[i][j];
+            for (int j = 0; j < count2; j++) {
+                System.out.print(answer[i][j]+ " ");
             }
+            System.out.println();
         }
 
         return answer;
     }
 }
 
-// for (int i = 0; i < arr1.length; i++) {
-//     for (int j = 0; j < arr2.length; j++) {
-//     System.out.println(answer[i][j]);
-//     }
-//     }
