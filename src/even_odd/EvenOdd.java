eb05e7a5739ee963adcf5c7f959b0421adfa480e
liverbird <ehdgns07@gmail.com>
2022-06-27 20:21:32 +0900

    Even & Odd complete

---
+++ src/even_odd/EvenOdd.java
@@ -0,0 +1,12 @@
+package even_odd;
+
+public class EvenOdd {
+    public String solution(int num) {
+
+        if( num%2 != 0 ){
+            return "Odd";
+        }
+
+        return "Even";
+    }
+}
