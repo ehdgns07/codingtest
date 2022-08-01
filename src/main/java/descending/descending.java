1d24164553b66502a51f76027d4c44fd41f24bf6
liverbird <ehdgns07@gmail.com>
2022-08-01 20:42:23 +0900

    1 solved

---
+++ src/main/java/descending/descending.java
@@ -0,0 +1,20 @@
+package descending;
+
+public class descending {
+
+    public static void main(String[] args) {
+        solution(12345L);
+    }
+    public static long solution(long n){
+        Long s = n;
+        String a = s.toString();
+        int k = a.length();
+        char[] p = a.toCharArray();
+
+        for(int i = 0; i<k ; i++){
+            
+        }
+
+        return s;
+    }
+}
